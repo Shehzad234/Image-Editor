99a3c286ec17da5472655d83ccbd41a8efb0de75
sadiqkhan61651 <trickswithandro@gmail.com>
2023-07-18 03:06:57 -0700

    first commit

---
+++ lib/features/landing/screen/landing_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange[50],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                  height: 200,
+                  width: 200,
+                  child: Image.network(
+                      "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/photo-editor-app-icon-design-template-4422455fe13e1c4e7008958f0b058eb5_screen.jpg?ts=1577021462")),
+            ),
+          ),
+          const Text(
+            "Image Editor",
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          SizedBox(height: 100),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ElevatedButton(
+                    onPressed: () {},
+                    child: const Icon(Icons.camera_alt_outlined)),
+                ElevatedButton(
+                    onPressed: () {}, child: const Icon(Icons.filter))
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
